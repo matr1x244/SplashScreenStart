877ee610ce90dbdd054f423a816ddccf855d7074
matr1x244 <zio-21@yandex.ru>
2022-07-01 21:52:15 +0300

    Добавил анимацию splash screen

--- app/src/main/java/com/geekbrains/splashscreenstart/MainActivity.kt
+++ app/src/main/java/com/geekbrains/splashscreenstart/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.geekbrains.splashscreenstart
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.geekbrains.splashscreenstart.databinding.ActivityMainBinding
 
@@ -14,6 +18,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val screen = installSplashScreen() // запускаем splash screen
         setContentView(binding.root)
+
+        /**
+         * Создаём анимацию для splash screen
+         */
+        screen.setOnExitAnimationListener{ screenProvider ->
+            ObjectAnimator.ofFloat(
+                screenProvider.view,
+                View.TRANSLATION_Y,
+                0f,
+                screenProvider.view.height.toFloat()
+            ).apply {
+                duration = 5000
+                interpolator = AnticipateInterpolator()
+                doOnEnd {
+                    screenProvider.remove()
+                }
+            }.start()
+        }
 
     }
 
